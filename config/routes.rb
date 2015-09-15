ea7785f3eee73c022083257e206394a6db8cc482
staciahoov <staciaray@roadrunner.com>
2015-09-15 17:04:50 +0000

    show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   get 'welcome/about'
-  root to: 'welcome#index'
+  root to: 'topics#index'
   post :incoming, to: 'incoming#create'
   
   resources :topics do
