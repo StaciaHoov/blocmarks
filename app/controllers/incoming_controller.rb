3f55af137a0daac3f8d09b19db0ce5d9e46af608
staciahoov <staciaray@roadrunner.com>
2015-09-17 22:49:59 +0000

    bookmark build edit

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -8,7 +8,7 @@ class IncomingController < ApplicationController
         if user
             topic = Topic.where(title: params[:subject]).first_or_create   # Find the topic by using params[:subject]
             url = params["body-plain"]   # Assign the url to a variable after retreiving it from params["body-plain"]
-            topic.bookmarks.create!(url: url, title: url)   # Now that you're sure you have a valid user and topic, build and save a new bookmark
+            topic.bookmarks.create!(user: user, url: url, title: url)   # Now that you're sure you have a valid user and topic, build and save a new bookmark
         end
         head 200
     end
