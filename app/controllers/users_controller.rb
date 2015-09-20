84d91bb2de466d59c788fe0b72233d64638b0f8c
staciahoov <staciaray@roadrunner.com>
2015-09-20 14:28:21 +0000

    user profile without topics

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
     
+    def show
+        @user = current_user
+        @user_bookmarks = @user.bookmarks
+        @liked_bookmarks = @user.likes
+       
+    end
+    
+    
     def create
         @user = User.new(params[:user])
         
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
 
   devise_for :users
+  resources :users, only: [:show]
   get 'welcome/about'
   root to: 'topics#index'
   post :incoming, to: 'incoming#create'
   
   resources :topics do
-    resources :bookmarks, except: [:index] do
-      resources :likes, only: [:create, :destroy]
-    end
+    resources :bookmarks, except: [:index, :show, :edit, :update] 
+  end
+  
+  resources :bookmarks do
+    resources :likes, only: [:create, :destroy]
   end
 end
+ 
